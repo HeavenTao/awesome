db927ea278099322c9ea8883495fec4f2908779e
HeavenTao <542248377@qq.com>
2023-12-06 22:08:06 +0800

    add timer

--- component/timer.lua
+++ component/timer.lua
@@ -1,22 +1,37 @@
 local wibox = require("wibox")
 local gears = require("gears")
 local theme = require("main.theme")
+local awful = require("awful")
+
+
+local textbox = wibox.widget {
+    markup = "<span color='" .. theme.text .. "'>" .. "</span>",
+    widget = wibox.widget.textbox
+};
 
 local widget = wibox.widget {
     {
         widget = wibox.container.margin,
         left = 10,
         right = 10,
-        {
-            widget = wibox.widget.textbox,
-            markup = "<span color='" .. theme.text .. "'>" .. "2023-12-06 16:33" .. "</span>"
-        },
+        textbox
     },
     bg = theme.bg,
     shape_border_width = 2,
     shape_border_color = theme.border,
     widget = wibox.container.background,
     shape = gears.shape.rounded_bar
+}
+
+gears.timer {
+    timeout = 1,
+    call_now = true,
+    autostart = true,
+    callback = function()
+        awful.spawn.easy_async({ "sh", "-c", "date '+%Y-%m-%d %H:%M'" }, function(out)
+            textbox.markup = "<span color='" .. theme.text .. "'>" .. out .. "</span>"
+        end)
+    end
 }
 
 return widget
