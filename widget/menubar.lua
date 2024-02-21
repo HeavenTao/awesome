c5073228b59683830ba99c4cea782b297896a842
HeavenTao <542248377@qq.com>
2024-02-21 21:12:15 +0800

    change prompt height

--- widget/menubar.lua
+++ widget/menubar.lua
@@ -2,30 +2,32 @@ local theme = require("catppuccin.mocha")
 local beautiful = require("beautiful")
 local vars = require("main.user-variables")
 local menubar = require("menubar")
+local dpi = require("beautiful.xresources").apply_dpi
+
 local _M = {
-    init = function(s)
-        -- Menubar configuration
-        -- Set the terminal for applications that require it
-        menubar.utils.terminal = vars.terminal
+	init = function(s)
+		-- Menubar configuration
+		-- Set the terminal for applications that require it
+		menubar.utils.terminal = vars.terminal
 
-        menubar.geometry = {
-            y = 0,
-            height = 40
-        }
+		menubar.geometry = {
+			y = 0,
+			height = dpi(25),
+		}
 
-        menubar.show_categories = false
-        beautiful.menubar_bg_normal = theme.base.hex
-        beautiful.menubar_fg_normal = theme.text.hex
+		menubar.show_categories = false
+		beautiful.menubar_bg_normal = theme.base.hex
+		beautiful.menubar_fg_normal = theme.text.hex
 
-        beautiful.menubar_bg_focus = theme.surface2.hex
-        beautiful.menubar_fg_focus = theme.text.hex
+		beautiful.menubar_bg_focus = theme.surface2.hex
+		beautiful.menubar_fg_focus = theme.text.hex
 
-        beautiful.prompt_bg = theme.base.hex
-        beautiful.prompt_fg = theme.text.hex
+		beautiful.prompt_bg = theme.base.hex
+		beautiful.prompt_fg = theme.text.hex
 
-        beautiful.prompt_bg_cursor = theme.base.hex
-        beautiful.prompt_fg_cursor = theme.text.hex
-    end
+		beautiful.prompt_bg_cursor = theme.base.hex
+		beautiful.prompt_fg_cursor = theme.text.hex
+	end,
 }
 
 return _M
